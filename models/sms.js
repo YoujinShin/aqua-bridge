b5b6f0250dce1814bd19b1d21904904d51099236
Youjin Shin <meggonagul@gmail.com>
2014-02-27 19:46:05 -0500

    editing

--- models/sms.js
+++ models/sms.js
@@ -3,15 +3,8 @@ var Schema = mongoose.Schema;
 
 // define astronaut schema
 var SmsSchema = new Schema({
-    slug : { type: String, lowercase: true, required: true, unique: true },
 	sender : String,
 	message : String,
-	photo: String,
-	qualitydata : {
-		colilert: Boolean,
-		petrifilm: String,
-		boilert: String
-	},
     lastupdated : { type: Date, default: Date.now },
 });
 
